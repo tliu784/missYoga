86f106d75a2b9581ca913003580725427f2d6948
benjamin1990 <benjamin.niu1990@gmail.com>
2014-03-07 18:10:06 -0800

    hide the recommendation

--- src/com/android/myhealthmate/MainPage.java
+++ src/com/android/myhealthmate/MainPage.java
@@ -2,15 +2,25 @@ package com.android.myhealthmate;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainPage extends Activity {
-	
+	private TextView rec_content;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.homepage);		
-		
-	}
-	
+		setContentView(R.layout.homepage);
 
+		rec_content = (TextView) findViewById(R.id.rec_content);
+		rec_content.setVisibility(View.GONE);
+
+	}
+
+	public void toggle_contents(View v) {
+
+		rec_content.setVisibility(rec_content.isShown() ? View.GONE
+				: View.VISIBLE);
+	}
 }
